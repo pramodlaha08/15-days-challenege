dd2fb920a87cf12239b07aead7e5cadc67ee65c1
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-09-16 15:53:40 +0545

    refactor: code modified

--- Day9_GithubRepoFinder/scripts/utility/githubRestApi.js
+++ Day9_GithubRepoFinder/scripts/utility/githubRestApi.js
@@ -7,8 +7,15 @@ export async function githubApi(value) {
          .then((data) => {
              return data.items
          })
+        .then((items) => {
+            console.log(items)
+            console.log(items.length)
+            let random = Math.floor(Math.random() * items.length);
+            console.log(random)
+            return items[random];
+        })
         .then((item) => {
-            console.log(item)
+            console.log(item.html_url);
         })
         .catch(e => {
             console.log('error fetaching data', e);
