5afbe72f101d4712eeabfc1bcc47d3d2f3f145b0
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-09-16 18:36:41 +0545

    feat: added event listener to display result

--- Day9_GithubRepoFinder/scripts/main.js
+++ Day9_GithubRepoFinder/scripts/main.js
@@ -1,9 +1,16 @@
 import { languages } from './utility/languages.js';
+import { githubApi } from './utility/githubRestApi.js';
 
-let select = document.querySelector('#language');
-console.log(select.innerHTML);
-// await languages().then((data) => {
-//     console.log(data)
-// })
+let selector = document.querySelector('select');
+let btn = document.querySelector('#find');
 
+btn.addEventListener('click', () => {
+    githubApi(selector.value);
+    gsap.from('.result', {
+        opacity: 0,
+        duration: 4,
+    })
+})
 languages();
+
+githubApi('javascript');
