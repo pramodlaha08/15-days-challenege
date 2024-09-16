afb5dd84ca5cf688f3e5e98864580a8f91bc07a6
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-09-16 18:48:28 +0545

    modified async await more efficiently to make call

--- Day9_GithubRepoFinder/scripts/main.js
+++ Day9_GithubRepoFinder/scripts/main.js
@@ -4,8 +4,8 @@ import { githubApi } from './utility/githubRestApi.js';
 let selector = document.querySelector('select');
 let btn = document.querySelector('#find');
 
-btn.addEventListener('click', () => {
-    githubApi(selector.value);
+btn.addEventListener('click', async() => {
+   await githubApi(selector.value);
     gsap.from('.result', {
         opacity: 0,
         duration: 4,
